2783b45a45d43718c982061bc25cf2df7a2cac5d
甲斐義隆 <katashiyoika@mba.local>
2021-02-07 15:48:07 +0900

    ふわっとアニメーションを導入

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 'use strict';
 {
-  const src_pic = ["img/contact.jpg", "img/drums.jpg", "img/keshiki.jpg"];
-  let num = -1;
-
-  function slideImage () {
-    // if (num === 2) {
-    //   num = 0;
-    // } else {
-    //   num ++; 
-    // }
-    num = Math.floor(Math.random() * src_pic.length)
-
-    document.getElementById("mypic").src = src_pic[num];
-  }
   
-  setInterval(slideImage, 2000);
+  // const src_pic = ["img/contact.jpg", "img/drums.jpg", "img/keshiki.jpg"];
+  // let num = -1;
+
+  // function slideImage () {
+  //   // if (num === 2) {
+  //   //   num = 0;
+  //   // } else {
+  //   //   num ++; 
+  //   // }
+  //   num = Math.floor(Math.random() * src_pic.length)
+
+  //   document.getElementById("mypic").src = src_pic[num];
+  // }
+  
+  // setInterval(slideImage, 2000);
+  $(function(){
+    $(".fade-img img:not(:first-child)").hide();
+    setInterval(function() {
+      $(".fade-img img:first-child").fadeOut("slow").next("img").fadeIn("slow").end().appendTo(".fade-img");
+    },6000);
+  });
 }
+
