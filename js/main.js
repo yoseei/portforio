636571809fcde77ef246da8a34a07995faa78f17
甲斐義隆 <katashiyoika@mba.local>
2021-02-07 13:55:45 +0900

    スライドショーを追加

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 'use strict';
 {
-  
+  const src_pic = ["img/contact.jpg", "img/drums.jpg", "img/keshiki.jpg"];
+  let num = -1;
+
+  function slideImage () {
+    if (num === 2) {
+      num = 0;
+    } else {
+      num ++; 
+    }
+    document.getElementById("mypic").src = src_pic[num];
+  }
+  setInterval(slideImage, 2000);
 }
